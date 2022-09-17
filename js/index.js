a12720c6357fbbb53ea7fed639ebaf8a3a24a5bb
Dmitri Gorun <iii448@mail.ru>
2022-09-17 14:48:01 +0500

    fifth commit

--- js/index.js
+++ js/index.js
@@ -126,3 +126,58 @@ countryWrapper.addEventListener('click', ({ target }) => {
     showPrice(target.dataset.currency);
   }
 });
+
+
+// timer
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+  0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const timer = deadline => {
+  const unitDay = document.querySelector('.timer__unit_day');
+  const unitHour = document.querySelector('.timer__unit_hour');
+  const unitMinute = document.querySelector('.timer__unit_minute');
+
+  const descriptionDay = document.querySelector('.timer__unit-description_day');
+  const descriptionHour = document.querySelector('.timer__unit-description_hour');
+  const descriptionMinute = document.querySelector('.timer__unit-description_minute');
+
+  const getTimeRemaning = () => {
+    const dateStop = new Date(deadline).getTime();
+    const dateNow = Date.now();
+    const timeRemaning = dateStop - dateNow;
+
+    // const ms = timeRemaning;
+    // const s = timeRemaning / 1000 % 60;
+    const minutes = Math.floor(timeRemaning / 1000 / 60 % 60);
+    const hours = Math.floor(timeRemaning / 1000 / 60 / 60 % 24);
+    // const d = timeRemaning / 1000 / 60 / 60 / 24 % 365;
+    const days = Math.floor(timeRemaning / 1000 / 60 / 60 / 24);
+
+    return { timeRemaning, minutes, hours, days };
+  };
+
+  const start = () => {
+    const timer = getTimeRemaning();
+
+    unitDay.textContent = timer.days;
+    unitHour.textContent = timer.hours;
+    unitMinute.textContent = timer.minutes;
+
+    descriptionDay.textContent = declOfNum(timer.days, ['день', 'дня', 'дней']);
+    descriptionHour.textContent = declOfNum(timer.hours, ['час', 'часа', 'часов']);
+    descriptionMinute.textContent = declOfNum(timer.minutes, ['минута', 'минуты', 'минут']);
+
+    const timerId = setTimeout(start, 60000);
+
+    if (timer.timeRemaning < 0) {
+      clearTimeout(timerId);
+      unitDay.textContent = '0';
+      unitHour.textContent = '0';
+      unitMinute.textContent = '0';
+    }
+  };
+
+  start();
+};
+
+timer('2023/09/07 20:00');
